70894f788d2623caea5ffa3a37ee296804f359c0
JuliaKeiter <75677685+JuliaKeiter@users.noreply.github.com>
2022-10-31 18:02:07 +0100

    änderung

--- Projekt 2/Julia/Projekt2_Julia_Keiter.R
+++ Projekt 2/Julia/Projekt2_Julia_Keiter.R
@@ -4,67 +4,67 @@ setwd("~/Documents/Uni/6. Semester/Fallstudien/Projekt 2")
 library(moments)
 library(car)
 library(xtable)
+install.packages("DAAG")
+library(DAAG)
 
 #Einlesung der Daten####
-MS <- read.csv("mietspiegel2015.csv", header = T, sep=" ", quote = "\"\"",
-               col.names = c("nm", "nmqm", "wfl", "rooms", "bj", "bez","wohngut",
+miete <- read.csv("mietspiegel2015.csv", header = T, sep=" ", quote = "\"\"",
+               col.names = c("nm", "nmqm", "wfl", "roomiete", "bj", "bez","wohngut",
                              "wohnbest", "ww0", "zh0", "badkach0", "badextra",
                              "kueche"))
 
-#Gruppe 1: Betrachtung der nm, nmqm fällt weg
-#MS <- MS[,-2]
+#Strukturierung der Variablen
+miete$wohngut[miete$wohngut == 1] <- "Gute Lage"
+miete$wohngut[miete$wohngut == 0] <- "andere Lagekategorie"
 
-'#Strukturierung der Variablen
-MS$wohngut[MS$wohngut == 1] <- "Gute Lage"
-MS$wohngut[MS$wohngut == 0] <- "andere Lagekategorie"
+miete$wohnbest[miete$wohnbest == 1] <- "Beste Lage"
+miete$wohnbest[miete$wohnbest == 0] <- "andere Lagekategorie"
 
-MS$wohnbest[MS$wohnbest == 1] <- "Beste Lage"
-MS$wohnbest[MS$wohnbest == 0] <- "andere Lagekategorie"
+miete$ww0[miete$ww0 == 1] <- "nein"
+miete$ww0[miete$ww0 == 0] <- "ja"
 
-MS$ww0[MS$ww0 == 1] <- "nein"
-MS$ww0[MS$ww0 == 0] <- "ja"
+miete$zh0[miete$zh0 == 1] <- "nein"
+miete$zh0[miete$zh0 == 0] <- "ja"
 
-MS$zh0[MS$zh0 == 1] <- "nein"
-MS$zh0[MS$zh0 == 0] <- "ja"
+miete$badkach0[miete$badkach0 == 1] <- "nicht gefliest"
+miete$badkach0[miete$badkach0 == 0] <- "gefliest"
 
-MS$badkach0[MS$badkach0 == 1] <- "nicht gefliest"
-MS$badkach0[MS$badkach0 == 0] <- "gefliest"
+miete$badextra[miete$badextra == 1] <- "gehoben"
+miete$badextra[miete$badextra == 0] <- "normal"
 
-MS$badextra[MS$badextra == 1] <- "gehoben"
-MS$badextra[MS$badextra == 0] <- "normal"
+miete$kueche[miete$kueche == 1] <- "gehoben"
+miete$kueche[miete$kueche == 0] <- "normal"
 
-MS$kueche[MS$kueche == 1] <- "gehoben"
-MS$kueche[MS$kueche == 0] <- "normal"
-'
 #Faktorisierung der nominalen Variablen
-MS$bez <- as.factor(MS$bez)
-MS$wohngut <- as.factor(MS$wohngut)
-MS$wohnbest <- as.factor(MS$wohnbest)
-MS$ww0 <- as.factor(MS$ww0)
-MS$zh0 <- as.factor(MS$zh0)
-MS$badkach0 <- as.factor(MS$badkach0)
-MS$badextra <- as.factor(MS$badextra)
-MS$kueche <- as.factor(MS$kueche)
+miete$bez <- as.factor(miete$bez)
+miete$wohngut <- as.factor(miete$wohngut)
+miete$wohnbest <- as.factor(miete$wohnbest)
+miete$ww0 <- as.factor(miete$ww0)
+miete$zh0 <- as.factor(miete$zh0)
+miete$badkach0 <- as.factor(miete$badkach0)
+miete$badextra <- as.factor(miete$badextra)
+miete$kueche <- as.factor(miete$kueche)
 
 
-str(MS)
-'data.frame:	3065 obs. of  12 variables:
- $ nm      : num  608 780 823 500 595 ...
- $ wfl     : int  48 60 110 58 70 81 97 50 71 76 ...
- $ rooms   : int  2 2 5 2 3 3 3 2 3 3 ...
- $ bj      : num  1958 1983 1958 1958 1972 ...
- $ bez     : Factor w/ 25 levels "Allach-Untermenzing",..: 25 6 15 20 4 20 8 11 25 25 ...
- $ wohngut : Factor w/ 2 levels "andere Lagekategorie",..: 1 2 1 1 1 1 2 2 1 1 ...
- $ wohnbest: Factor w/ 2 levels "andere Lagekategorie",..: 1 1 1 1 1 1 1 1 1 1 ...
- $ ww0     : Factor w/ 2 levels "ja","nein": 1 1 1 1 1 1 1 1 1 1 ...
- $ zh0     : Factor w/ 2 levels "ja","nein": 1 1 2 1 1 1 1 1 1 2 ...
- $ badkach0: Factor w/ 2 levels "gefliest","nicht gefliest": 2 2 2 2 1 2 2 1 2 2 ...
- $ badextra: Factor w/ 2 levels "gehoben","normal": 2 2 1 2 2 2 1 2 2 1 ...
- $ kueche  : Factor w/ 2 levels "gehoben","normal": 2 1 2 1 2 2 1 1 2 2 ...'
+str(miete)
+'data.frame:	3065 obs. of  13 variables:
+$ nm      : num  608 780 823 500 595 ...
+$ nmqm    : num  12.67 13 7.48 8.62 8.5 ...
+$ wfl     : int  48 60 110 58 70 81 97 50 71 76 ...
+$ roomiete   : int  2 2 5 2 3 3 3 2 3 3 ...
+$ bj      : num  1958 1983 1958 1958 1972 ...
+$ bez     : Factor w/ 25 levels "Allach-Untermenzing",..: 25 6 15 20 4 20 8 11 25 25 ...
+$ wohngut : Factor w/ 2 levels "0","1": 1 2 1 1 1 1 2 2 1 1 ...
+$ wohnbest: Factor w/ 2 levels "0","1": 1 1 1 1 1 1 1 1 1 1 ...
+$ ww0     : Factor w/ 2 levels "0","1": 1 1 1 1 1 1 1 1 1 1 ...
+$ zh0     : Factor w/ 2 levels "0","1": 1 1 2 1 1 1 1 1 1 2 ...
+$ badkach0: Factor w/ 2 levels "0","1": 2 2 2 2 1 2 2 1 2 2 ...
+$ badextra: Factor w/ 2 levels "0","1": 1 1 2 1 1 1 2 1 1 2 ...
+$ kueche  : Factor w/ 2 levels "0","1": 1 2 1 2 1 1 2 2 1 1 ...'
 
 #Datenqualität####
 #insgesamt
-sum(is.na(MS))
+sum(is.na(miete))
 #[1] 0
 
 
@@ -91,12 +91,18 @@ erstell_tabelle <- function(daten){
   return(speicher)
 }
 
-lapply(MS[,1:4], erstell_tabelle)
+lapply(miete[,1:5], erstell_tabelle)
 '$nm
 arithm. Mittel         Median        Minimum        Maximum     Spannweite      1.Quartil 
     763.063396     700.000000     174.750000    6000.000000    5825.250000     550.000000 
      3.Quartil            IQR   Standardabw.            MAD        Schiefe        Wölbung 
     910.460000     360.460000     338.162672     261.901290       2.589191      25.474844 
+
+$nmqm
+arithm. Mittel         Median        Minimum        Maximum     Spannweite      1.Quartil      3.Quartil            IQR 
+   10.73159869    10.84000000     2.47000000    22.13000000    19.66000000     9.03000000    12.45000000     3.42000000 
+  Standardabw.            MAD        Schiefe        Wölbung 
+    2.67451983     2.50559400     0.04389988     3.33967955 
 
 $wfl
 arithm. Mittel         Median        Minimum        Maximum     Spannweite      1.Quartil 
@@ -104,7 +110,7 @@ arithm. Mittel         Median        Minimum        Maximum     Spannweite      
      3.Quartil            IQR   Standardabw.            MAD        Schiefe        Wölbung 
      85.000000      30.000000      25.742312      22.239000       1.351719       8.334088 
 
-$rooms
+$roomiete
 arithm. Mittel         Median        Minimum        Maximum     Spannweite      1.Quartil 
      2.7014682      3.0000000      1.0000000      8.0000000      7.0000000      2.0000000 
      3.Quartil            IQR   Standardabw.            MAD        Schiefe        Wölbung 
@@ -117,7 +123,7 @@ arithm. Mittel         Median        Minimum        Maximum     Spannweite      
   1983.0000000     25.5000000     26.5054412     27.4281000     -0.1823192      2.3144530 '
 
 #nominale Variablen
-lapply(MS[,6:12], table)
+lapply(miete[,6:13], table)
 '$bez
         Allach-Untermenzing              Altstadt-Lehel               Au-Haidhausen 
                          25                          47                         167 
@@ -168,42 +174,53 @@ gehoben  normal
 
 
 #Ausreißer: nm=6000 entfernen
-MS <- subset(MS, nm!=6000.00)
+#miete <- subset(miete, nm!=6000.00)
 
 
-#erster Blick auf mögliche Abhängigkeiten von nm zu anderen Variablen
-MS.lm <- lm(nm ~ ., data = MS) # Regressant: nm, Regressor: alle anderen Variablen
+#Modellbildung
+mietelm1 <- lm(nm ~ ., data = miete[,-2]) # Regressant: nm, Regressor: alle anderen Variablen
 
-carPalette()
+summary(mietelm1)
 
-scatterplotMatrix(~ . | nm, data=MS, col="slategray3")
-'Warning message:
-In scatterplotMatrix.default(X[, -ncol], groups = X[, ncol], ...) :
-  number of groups exceeds number of available colors
-colors are recycled' 
-#funktioniert trotzdem
+par(mfrow=c(2,3))
+plot(mietelm1, which = 1) #residual vs fitted
+plot(mietelm1, which = 2) #q-q-plots
+plot(mietelm1, which = 3) #scale-location
+plot(mietelm1, which = 4) #cooks distance
+plot(mietelm1, which = 5) #leverage
+plot(mietelm1, which=6)
+
+#Multikollinearität
+#über Determinante
+det(t(model.matrix(mietelm1))%*%model.matrix(mietelm1))
+
+#über VIF
+DAAG::vif(mietelm1)
 
 #Latex Ausgabe
 
 #Tabelle 1: metrische Variablen 
 xtable(caption = "univariate Kenngrößen für metrische Variablen",
-       cbind("Nettomiete (€)" = erstell_tabelle(na.omit(MS$nm)),
-             "Wohnfläche (qm)" = erstell_tabelle(na.omit(MS$wfl)),
-             "Zimmeranzahl" = erstell_tabelle(na.omit(MS$rooms)),
-             "Baujahr" = erstell_tabelle(na.omit(MS$bj))),
+       cbind("Nettomiete (€)" = erstell_tabelle(na.omit(miete$nm)),
+             "Wohnfläche (qm)" = erstell_tabelle(na.omit(miete$wfl)),
+             "Zimmeranzahl" = erstell_tabelle(na.omit(miete$roomiete)),
+             "Baujahr" = erstell_tabelle(na.omit(miete$bj))),
        digits = 2)
 
 #Tabelle 2: dichotome  Variablen
 
 xtable(caption = "Deskriptive Kenngrößen für dichotome Variablen",
-       cbind("Gute Lage"= c(table(MS$wohngut), table(MS$wohngut)/length(MS$wohngut)),
-             "Beste Lage" = c(table(MS$wohnbest), table(MS$wohnbest)/length(MS$wohnbest)),
-             "zentrale Warmwasserversorgung" = c(table(MS$ww0), table(MS$ww0)/length(MS$ww0)),
-             "Zentralheizung" =c(table(MS$zh0), table(MS$zh0)/length(MS$zh0)),
-             "Gefliestes Bad" =c(table(MS$badkach0), table(MS$badkach0)/length(MS$badkach0)),
-             "Badausstattung" =c(table(MS$badextra), table(MS$badextra)/length(MS$badextra)),
-             "Küchenaustattung" =c(table(MS$kueche), table(MS$kueche)/length(MS$kueche))))
+       cbind("Gute Lage"= c(table(miete$wohngut), table(miete$wohngut)/length(miete$wohngut)),
+             "Beste Lage" = c(table(miete$wohnbest), table(miete$wohnbest)/length(miete$wohnbest)),
+             "zentrale Warmwasserversorgung" = c(table(miete$ww0), table(miete$ww0)/length(miete$ww0)),
+             "Zentralheizung" =c(table(miete$zh0), table(miete$zh0)/length(miete$zh0)),
+             "Gefliestes Bad" =c(table(miete$badkach0), table(miete$badkach0)/length(miete$badkach0)),
+             "Badausstattung" =c(table(miete$badextra), table(miete$badextra)/length(miete$badextra)),
+             "Küchenaustattung" =c(table(miete$kueche), table(miete$kueche)/length(miete$kueche))))
 
 #Abbildung 1: Boxplot Nettomiete
 rand <- par(mar = c(20, 4, 2, 2) + 0.2)
-boxplot(MS$nm, col = "slategray2",xlab = "Nettomiete in Euro", horizontal = T)
+boxplot(miete$nm, col = "slategray2",xlab = "Nettomiete in Euro", horizontal = T)
+par(rand)
+
+
