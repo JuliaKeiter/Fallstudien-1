ba36cd8f454525269294f58c97313843b51929e1
louisap12 <94864182+louisap12@users.noreply.github.com>
2022-11-07 21:07:29 +0100

    Vorwärts- und Rückwärtselimination (ohne rooms!) + Zusammenfassung wohnlage

--- Projekt 2/Projekt 2_all.R
+++ Projekt 2/Projekt 2_all.R
@@ -33,7 +33,7 @@ miete$zh0 <- as.factor(miete$zh0)
 miete$badkach0 <- as.factor(miete$badkach0)
 miete$badextra <- as.factor(miete$badextra)
 miete$kueche <- as.factor(miete$kueche)
-miete$bez <- gsub("ö", "�", miete$bez)
+miete$bez <- gsub("Ã¶", "ö", miete$bez)
 miete$bez <- as.factor(miete$bez)
 
 # Deskriptive Kenngroessen zu den numerischen Variablen:
@@ -53,7 +53,7 @@ erstell_tabelle <- function(daten){
   speicher[12] <- kurtosis(daten)
   names(speicher) <- c("arithm. Mittel", "Median", "Minimum", "Maximum",
                        "Spannweite", "1.Quartil", "3.Quartil", "IQR",
-                       "Standardabw.", "MAD","Schiefe", "W�lbung")
+                       "Standardabw.", "MAD","Schiefe", "Wölbung")
   return(speicher)
 }
 
@@ -164,5 +164,161 @@ plot(mietelm1, which = 5) #leverage
 det(t(model.matrix(mietelm1))%*%model.matrix(mietelm1)) #multikollineariteat
 
 step(mietelm1)
-#AIC, BIC, adjustiertes Bestimmtheitsma�, Mallow Cp
+#AIC, BIC, adjustiertes Bestimmtheitsmaß, Mallow Cp
 #vorwaertsselektion, rueckwaertselimination, schrittweise regression
+
+###############################################################################################################################################
+
+# Wohnlage zusmmenfassen
+miete$wohngut[which(miete$wohnbest  == 1)]
+miete$wohnbest[which(miete$wohngut  == 1)]
+
+miete$wohnlage <- rep(0, length(miete$wohngut))
+miete$wohnlage[which(miete$wohnbest == 1)] <- "beste"
+miete$wohnlage[which(miete$wohngut == 1)] <- "gute"
+miete$wohnlage[which(miete$wohnlage == 0)] <- "andere"
+
+miete$wohnlage <- as.factor(miete$wohnlage)
+
+
+# Rueckwaertelimination (ohne rooms)
+M8 <- lm(nm ~ wfl + bj + bez + wohnlage + ww0 + zh0
+         + badkach0 + badextra + kueche, data = miete )
+M8
+summary(M8)
+# Es kann keine Variable aufgrund des p-Wertes eliminiert werden
+
+
+
+# Vorwaertselimination (ohne rooms)
+
+m1 <- lm(nm ~ wfl, data = miete )
+m2 <- lm(nm ~ bj, data = miete )
+m3 <- lm(nm ~ bez, data = miete )
+m4 <- lm(nm ~ wohnlage, data = miete )
+m5 <- lm(nm ~ ww0, data = miete )
+m6 <- lm(nm ~ zh0, data = miete )
+m7 <- lm(nm ~ badkach0, data = miete )
+m8 <- lm(nm ~ badextra, data = miete )
+m9 <- lm(nm ~ kueche, data = miete )
+
+summary(m1)$coefficients
+summary(m2)$coefficients
+summary(m3)$coefficients
+summary(m4)$coefficients
+summary(m5)$coefficients
+summary(m6)$coefficients
+summary(m7)$coefficients
+summary(m8)$coefficients
+summary(m9)$coefficients
+
+# wfl aufnehmen
+
+m1 <- lm(nm ~ wfl + bj, data = miete )
+m2 <- lm(nm ~ wfl + bez , data = miete )
+m3 <- lm(nm ~ wfl + wohnlage, data = miete )
+m4 <- lm(nm ~ wfl + ww0, data = miete )
+m5 <- lm(nm ~ wfl + zh0, data = miete )
+m6 <- lm(nm ~ wfl + badkach0, data = miete )
+m7 <- lm(nm ~ wfl + badextra, data = miete )
+m8 <- lm(nm ~ wfl + kueche, data = miete )
+
+summary(m1)$coefficients
+summary(m2)$coefficients
+summary(m3)$coefficients
+summary(m4)$coefficients
+summary(m5)$coefficients
+summary(m6)$coefficients
+summary(m7)$coefficients
+summary(m8)$coefficients
+
+# kueche hinzufuegen
+
+m1 <- lm(nm ~ wfl + kueche + bj, data = miete )
+m2 <- lm(nm ~ wfl + kueche + bez , data = miete )
+m3 <- lm(nm ~ wfl + kueche + wohnlage, data = miete )
+m4 <- lm(nm ~ wfl + kueche + ww0, data = miete )
+m5 <- lm(nm ~ wfl + kueche + zh0, data = miete )
+m6 <- lm(nm ~ wfl + kueche + badkach0, data = miete )
+m7 <- lm(nm ~ wfl + kueche + badextra, data = miete )
+
+summary(m1)$coefficients
+summary(m2)$coefficients
+summary(m3)$coefficients
+summary(m4)$coefficients
+summary(m5)$coefficients
+summary(m6)$coefficients
+summary(m7)$coefficients
+
+# wohnlage hinzufuegen
+
+m1 <- lm(nm ~ wfl + kueche + wohnlage +  bj, data = miete )
+m2 <- lm(nm ~ wfl + kueche + wohnlage + bez , data = miete )
+m4 <- lm(nm ~ wfl + kueche + wohnlage + ww0, data = miete )
+m5 <- lm(nm ~ wfl + kueche + wohnlage + zh0, data = miete )
+m6 <- lm(nm ~ wfl + kueche + wohnlage + badkach0, data = miete )
+m7 <- lm(nm ~ wfl + kueche + wohnlage + badextra, data = miete )
+
+summary(m1)$coefficients
+summary(m2)$coefficients
+summary(m4)$coefficients
+summary(m5)$coefficients
+summary(m6)$coefficients
+summary(m7)$coefficients
+
+# baujahr hinzufuegen
+
+m2 <- lm(nm ~ wfl + kueche + wohnlage + bj + bez , data = miete )
+m4 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0, data = miete )
+m5 <- lm(nm ~ wfl + kueche + wohnlage + bj + zh0, data = miete )
+m6 <- lm(nm ~ wfl + kueche + wohnlage + bj + badkach0, data = miete )
+m7 <- lm(nm ~ wfl + kueche + wohnlage + bj + badextra, data = miete )
+
+summary(m2)$coefficients
+summary(m4)$coefficients
+summary(m5)$coefficients
+summary(m6)$coefficients
+summary(m7)$coefficients
+
+# ww01 hinzufuegen
+
+m4 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + bez, data = miete )
+m5 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + zh0, data = miete )
+m6 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0, data = miete )
+m7 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badextra, data = miete )
+
+summary(m4)$coefficients
+summary(m5)$coefficients
+summary(m6)$coefficients
+summary(m7)$coefficients
+
+# badkacheln
+
+m4 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0 + bez , data = miete )
+m5 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0 + zh0 , data = miete )
+m7 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0 + badextra , data = miete )
+
+summary(m4)$coefficients
+summary(m5)$coefficients
+summary(m7)$coefficients
+
+# zh0
+
+m4 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0 + zh0 + bez , data = miete )
+m7 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0 + zh0 + badextra , data = miete )
+
+summary(m4)$coefficients
+summary(m7)$coefficients
+
+
+# bez hinzunehmen
+
+m7 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0 + zh0 + bez + badextra , data = miete )
+
+summary(m7)$coefficients
+
+# badextra hinzufeugen
+
+m7 <- lm(nm ~ wfl + kueche + wohnlage + bj + ww0 + badkach0 + zh0 + bez + badextra + rooms , data = miete )
+summary(m7)
+plot(m7)
